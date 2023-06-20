4bf11e67accc84a26a627c044c58f920323d38db
BaronessLyfe <lyfechami@gmail.com>
2023-06-20 15:40:53 -0700

    Adding 0-putchar.c file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
-#define main.h
+#ifndef main_h
+#define main_h
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -13,3 +14,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
+#endif
