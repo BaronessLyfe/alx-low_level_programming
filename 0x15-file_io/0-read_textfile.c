2bc10a719e5d2288916113450d66e67b9cb52404
BaronessLyfe <lyfechami@gmail.com>
2023-08-11 16:58:41 -0700

    Adding 0-read_textfile.c file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 #include <stdlib.h>
 
@@ -5,24 +6,22 @@
 * read_textfile- Read text file print to STDOUT.
 * @filename: text file being read
 * @letters: number of letters to be read
-* Return: w- actual number of bytes read and printed,NULL=0
-*
+* Return: w- actual number of bytes read and printed,NULL = 0
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 char *buf;
 ssize_t lc;
-ssize_t v;
-ssize_t q;
-
-fd = open(filename, O_RDONLY);
+ssize_t w;
+ssize_t t;
+lc = open(filename, O_RDONLY);
 if (lc == -1)
 return (0);
 buf = malloc(sizeof(char) * letters);
-t
-q = read(fd, buf, letters);
-v = write(STDOUT_FILENO, buf, q);
+t = read(lc, buf, letters);
+w = write(STDOUT_FILENO, buf, t);
+
 free(buf);
-close(lc);     
-return (v;
+close(lc);
+return (w);
 }
