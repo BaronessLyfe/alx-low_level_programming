8e1763db98b5f48d82ab85e13fe53d9d1d33779f
BaronessLyfe <lyfechami@gmail.com>
2023-08-11 16:37:26 -0700

    Adding 0x15. C - File I/O files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,22 +5,24 @@
 * read_textfile- Read text file print to STDOUT.
 * @filename: text file being read
 * @letters: number of letters to be read
-* Return: w- actual number of bytes read and printed,otherwise 0.
+* Return: w- actual number of bytes read and printed,NULL=0
+*
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int lc;
-ssize_t c, v;
-char *buf
+char *buf;
+ssize_t lc;
+ssize_t v;
+ssize_t q;
 
-lc= open(filename, O_RDONLY);
+fd = open(filename, O_RDONLY);
 if (lc == -1)
 return (0);
-buffer = malloc(sizeof(char) * letters);
-c= read(lc, buf, letters)
-v = write(STDOUT_FILENO, buf, c);
-
+buf = malloc(sizeof(char) * letters);
+t
+q = read(fd, buf, letters);
+v = write(STDOUT_FILENO, buf, q);
 free(buf);
-close(lc);
-return (v);
+close(lc);     
+return (v;
 }
