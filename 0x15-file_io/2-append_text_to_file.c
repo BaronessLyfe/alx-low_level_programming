95d1076cdddb2f21c25ac4a3ba2c19936755a9db
Lyfe <107996166+BaronessLyfe@users.noreply.github.com>
2024-01-17 22:12:01 +0300

    Adding 0x15-file_io files

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* append_text_to_file - Appends text at the end of a file.
+* @filename: A pointer to the name of the file.
+* @text_content: The string to add to the end of the file.
+*
+* Return: If the function fails or filename is NULL =-1
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+int g, w, len = 0;
+if (filename == NULL)
+return (-1);
+if (text_content != NULL)
+{
+for (len = 0; text_content[len];)
+len++;
+}
+g = open(filename, O_WRONLY | O_APPEND);
+w = write(g, text_content, len);
+if (g == -1 || w == -1)
+return (-1);
+close(g);
+return (1);
+}
